dc3044dc06509d46b291e9d6e7dce46359f3e4fd
PauloCordeiro21 <pauloacorneto@gmail.com>
2025-07-29 09:45:49 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-function criacartao (categoria, pergunta, resposta) {
-let container = document.getElementById('container')
-let cartao = document.createElement('article')
-cartao.className = 'cartao'
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
-cartao.innerHTML = `
-<div class="cartao conteudo">
-<h3>${categoria}</h3>
-<div class="cartao__conteudo__pergunta">
-<p>${pergunta}</p>
-</div>
-<div class="cartao__conteudo__resposta">
-<p>${resposta}</p>
-</div>
-</div>
-`
-container.appendChild(cartao)
+    cartao.innerHTML = `
+    <div class="cartao_conteudo">
+    <h3>${categoria}</h3>
+    <div class="cartao_conteudo_pergunta">
+    <p>${pergunta}</p>
+    </div>
+    <div class="cartao_conteudo_resposta">
+    <p>${resposta}</p>
+    </div>
+    </div>
+    `
+    let respostaEstaVisivel = false;
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel;
+        cartao.classList.toggle('active', respostaEstaVisivel);
+    }
 
-let respostaEstaVisivel = false;
-function viracartao(){
-respostaEstaVisivel = !respostaEstavisivel;
-cartao.classList.toggle('active', respostaEstaVisivel)
-}
-cartao.addEventListener("click", viracartao)
-container.appendChild(cartao)
+    cartao.addEventListener("click", viraCartao);
+    container.appendChild(cartao)
 }
---
+++ pergunta.js
@@ -0,0 +1,18 @@
+criaCartao (
+    'Geografia',
+    'Qual o maior pais em extenção territorial do mundo?',
+    'Russia'
+    )
+    
+    criaCartao (
+    'Geografia',
+    'Qual é a capital da Russia?',
+    'A capital da Russia é Moscou'
+    )
+    
+    criaCartao (
+    'Geografia',
+    'Qual pais tem mais influencia no mundo?',
+    'O pais com mais influencia no mundo é os Estados Unidos'
+    )
+    
